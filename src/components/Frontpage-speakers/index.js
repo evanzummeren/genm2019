7119730ba853846ef7a36f50b6392890af4a2b54
Zaturrby <rjkorteschiel@gmail.com>
2018-04-07 15:40:58 +0200

    first GraphiQL query in

--- src/components/Frontpage-speakers/index.js
+++ src/components/Frontpage-speakers/index.js
@@ -2,15 +2,32 @@ import React from 'react'
 
 import styles from './index.module.css';
 
-import Speaker from './Speaker.js'
-import speakers from './speakers.yaml';
+import keith from './keith.png';
 
-const FrontpageSpeakers = () => (
-  <section>
-    { speakers.map((speaker, key) => {
+const SpeakerImg = () => (
+  <div>
+    <img src={keith} className={styles.image}></img>
+  </div>
+)
+
+const Speaker = (props) => (
+  <div className={styles.container}>
+    { props.speaker.inverseImg && <SpeakerImg />}
+    <div className={styles.block}>
+      <h1 className={styles.title}>{props.speaker.node.frontmatter.name}</h1>
+      <h2 className={styles.subtitle}>{props.speaker.node.frontmatter.function}</h2>
+      <div className={styles.text} dangerouslySetInnerHTML={{__html: props.speaker.node.html}}></div>
+    </div>
+    { !props.speaker.inverseImg && <SpeakerImg />}
+  </div>
+)
+
+const FrontpageSpeakers = (props) => {
+  console.log(props.speakers)
+  return <section>
+    { props.speakers.map((speaker, key) => {
       return <Speaker speaker={speaker} key={key}/>;
     })}
-  </section>
-)
+  </section>}
 
 export default FrontpageSpeakers
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,16 +11,37 @@ import FrontpageCasestudies from "../components/Frontpage-casestudies";
 import FrontpageDebat from "../components/Frontpage-debat";
 import FrontpageLocation from "../components/Frontpage-location";
 
-const IndexPage = () => (
-  <div>
-    <FrontpageHeader />
-    <FrontpageLanding />
-    <FrontpageSpeakers />
-    <FrontpageCasestudies />
-    <FrontpageDebat />
-    <FrontpageLocation />
-    <FrontpageCta />
-  </div>
-)
+const IndexPage = ({data}) => {
+  return (<div>
+      <FrontpageHeader />
+      <FrontpageLanding />
+      <FrontpageSpeakers speakers={data.speakers.edges}/>
+      <FrontpageCasestudies />
+      <FrontpageDebat />
+      <FrontpageLocation />
+      <FrontpageCta />
+    </div>);
+};
 
 export default IndexPage
+
+export const query = graphql`
+  query speakers {
+    speakers: allMarkdownRemark (
+          filter: {id: {regex: "//home/speakers//"}}
+    ){
+      edges {
+        node {
+          id
+          frontmatter {
+            name
+            function
+            image
+            inverseImg
+          }
+          html
+        }
+      }
+    }
+  }
+`;
