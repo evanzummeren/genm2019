1e7232b259a5bfc59f51b527e4cc0919d976c39e
Zaturrby <rjkorteschiel@gmail.com>
2018-04-07 18:37:17 +0200

    Yaaaaaay

--- src/components/Frontpage-speakers/index.js
+++ src/components/Frontpage-speakers/index.js
@@ -2,25 +2,25 @@ import React from 'react'
 import Img from "gatsby-image";
 
 import styles from './index.module.css';
-import keith from './keith.png';
 
-const SpeakerImg = () => (
-  <div>
-    <img src={keith} className={styles.image}></img>
-  </div>
-)
+const SpeakerImg = (props) => {
+  return (<div>
+    <img src={props.img.childImageSharp.responsiveSizes.src} className={styles.image}></img>
+  </div>);
+}
 
-const Speaker = (props) => (
-  <div className={styles.container}>
-    { props.speaker.node.frontmatter.inverseImg && <SpeakerImg />}
+const Speaker = (props) => {
+  // console.log(props.speaker)
+  return (<div className={styles.container}>
+    { props.speaker.node.frontmatter.inverseImg && <SpeakerImg img={props.speaker.node.frontmatter.featuredImage}/>}
     <div className={styles.block}>
       <h1 className={styles.title}>{props.speaker.node.frontmatter.name}</h1>
       <h2 className={styles.subtitle}>{props.speaker.node.frontmatter.function}</h2>
       <div className={styles.text} dangerouslySetInnerHTML={{__html: props.speaker.node.html}}></div>
     </div>
-    { !props.speaker.node.frontmatter.inverseImg && <SpeakerImg />}
-  </div>
-)
+    { !props.speaker.node.frontmatter.inverseImg && <SpeakerImg img={props.speaker.node.frontmatter.featuredImage}/>}
+  </div>);
+}
 
 const FrontpageSpeakers = (props) => {
   return <section>
