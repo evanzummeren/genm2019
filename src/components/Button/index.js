02246c81d36aee46916fd29d081fe3b2b5e2ef45
Zaturrby <rjkorteschiel@gmail.com>
2018-04-10 15:40:53 +0200

    marquee start

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,11 +4,9 @@ import classNames from 'classnames'
 import styles from "./index.module.css";
 
 const Button = (props) => {
-  console.log(props.alternative)
-  console.log(props.alternative && styles.alternative)
-  return (<div className={classNames(styles.container, props.alternative && styles.alternative)}>
+  return (<a href="http://nytconferences.com/" className={classNames(styles.container, props.alternative && styles.alternative)}>
     <p className={styles.text}>{props.text}</p>
-  </div>)
+  </a>)
 }
 
 export default Button
--- src/components/Rolling-title/index.js
+++ src/components/Rolling-title/index.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
+import Marquee from 'marquee-react-dwyer';
+
 import styles from "./index.module.css";
 
-const RollingTitle = (props) => (
-  <div className={styles.container}>
-    <h1>{props.title}</h1>
-  </div>
-)
+const RollingTitle = (props) => {
+  return (<div className={styles.container}>
+    <Marquee
+        Size={"h1"}
+        NumberOfOptions={"1"}
+        Index0={"DEBAT DEBAT DEBAT DEBAT DEBAT DEBAT DEBAT DEBAT DEBAT DEBAT DEBAT DEBAT"}
+        TimeToCross={"10000"}
+        TimeToChange={"2000"}
+        IsRandom={"true"}
+        Color={"white"}
+      />
+  </div>)
+}
 
 export default RollingTitle
+//
+// {Array.apply(null, { length: 10 }).map((value, key) => {
+//   return <h1 key={key}>{props.title}</h1>
+// })}
