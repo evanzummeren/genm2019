eb0677a30e246b92d62540107469e484513db3fb
Zaturrby <rjkorteschiel@gmail.com>
2018-04-11 15:09:31 +0200

    some typography

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -7,8 +7,10 @@ const Card = (props) => {
   const data = props.data.node;
   return (<div className={styles.container}>
     <div className={styles.card}>
-      <h1 className={styles.title}>{data.frontmatter.name}</h1>
-      <h2 className={styles.subtitle}>{data.frontmatter.function}</h2>
+      <div className={styles.titleWrapper}>
+        <h2 className={styles.title}>{data.frontmatter.name}</h2>
+        <h3 className={styles.subtitle}>{data.frontmatter.function}</h3>
+      </div>
       <div className={styles.text} dangerouslySetInnerHTML={{__html: data.html}}></div>
       {data.frontmatter.quote && <h2>{data.frontmatter.quote}</h2>}
       {data.frontmatter.featuredImage && <Img resolutions={data.frontmatter.featuredImage.childImageSharp.resolutions} />}
