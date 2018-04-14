a560bf282fb2d722f273086b0fa169c92fdbea2f
Zaturrby <rjkorteschiel@gmail.com>
2018-04-14 16:37:53 +0200

    another typography snipe

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -5,7 +5,7 @@ import styles from "./index.module.css";
 
 const Button = (props) => {
   return (<a href="http://www.expertisecentrumjournalistiek.nl/agenda/19-juni-2018-de-grote-expertisedag-nieuwe-media/" className={classNames(styles.container, props.alternative && styles.alternative)}>
-    <p className={styles.text}>{props.text}</p>
+    <h4 className={styles.text}>{props.text}</h4>
   </a>)
 }
 
