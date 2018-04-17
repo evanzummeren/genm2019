2cd3eacbdbba83fa7a12fe274edc990a7bca9b16
Zaturrby <rjkorteschiel@gmail.com>
2018-04-17 16:12:01 +0200

    change capitalisation

--- src/components/dialog/index.js
+++ src/components/dialog/index.js
@@ -3,11 +3,11 @@ import Button from "../Button";
 
 import styles from "./index.module.css";
 
-const dialog = (props) => (
+const Dialog = (props) => (
   <section className={styles.container}>
     <h4 className={styles.text}>{props.children}</h4>
     <Button text="Bestellen" alternative="true" />
   </section>
 )
 
-export default dialog
+export default Dialog
