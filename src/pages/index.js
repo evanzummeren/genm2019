85a7d198978f0a0b41f199ecf6b61915e1d9dc3a
Zaturrby <rjkorteschiel@gmail.com>
2018-04-18 15:37:33 +0200

    browser checking test

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,30 +25,33 @@ const styles = {
 import logo from "./logo.svg"
 import nose from "./nose.svg"
 
-
 import { Component, Property } from 'immutable-ics'
 const generateCalendar = (events) => {
-  console.log(events)
-
-  // Calendar basics
+  // Calendar setup
   let calendar
   const versionProperty = new Property({ name: 'VERSION', value: 2 })
   calendar = new Component({ name: 'VCALENDAR' })
   calendar = calendar.pushProperty(versionProperty)
 
-  // Events
+  // Events setup and push
   events.map((eventData, key)=>{
     let event
-    const dtstartProperty = new Property({
+    const startTimeProperty = new Property({
       name: 'DTSTART',
       parameters: { VALUE: 'DATETIME' },
-      value: new Date(eventData.node.frontmatter.time)
+      value: new Date(eventData.node.frontmatter.startTime)
+    })
+    const endTimeProperty = new Property({
+      name: 'DTEND',
+      parameters: { VALUE: 'DATETIME' },
+      value: new Date(eventData.node.frontmatter.endTime)
     })
     event = new Component({ name: 'VEVENT' })
-    event = event.pushProperty(dtstartProperty)
+    event = event.pushProperty(startTimeProperty, endTimeProperty)
     calendar = calendar.pushComponent(event)
   })
 
+  // Conversion to file
   return 'data:text/plain;charset=utf-8,' + encodeURIComponent(calendar.toString());
 }
 
