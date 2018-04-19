63f32b8bab38affaae0d7d9c3c203ced48694c90
Zaturrby <rjkorteschiel@gmail.com>
2018-04-19 15:36:34 +0200

    add 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,44 @@
 import React from 'react'
+import classNames from 'classnames'
+import Link from "gatsby-link"
 
-const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
-)
+import logo from "./landingpage/logo.svg"
+
+import stylesGrid from "./landingpage/grid.module.css"
+import stylesHeader from "./landingpage/header.module.css"
+const styles = {
+  ...stylesGrid,
+  ...stylesHeader
+}
+
+const NotFoundPage = () => {
+  return (
+    <div className={styles.screen}>
+      <div
+        className={styles.page}
+        style={{
+          display: 'flex',
+          flexDirection: 'column'
+        }}>
+        <header
+          className={classNames(styles.grid, styles.header)}
+          style={{
+            flexGrow: 0
+          }}>
+          <img className={styles.logo} src={logo}/>
+        </header>
+        <div
+          style={{
+            flexGrow: 1,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingBottom: '10rem'
+          }}
+        >
+          <h1>Deze pagina bestaat helaas niet, klik <Link to="">hier</Link></h1>
+        </div>
+      </div>
+    </div>)}
 
 export default NotFoundPage
