4d0d45ef6d2c2327e5cbad4955f0f4c5ab596993
Zaturrby <rjkorteschiel@gmail.com>
2018-04-21 12:08:37 +0200

    favicon location

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
             {
               resolve: `gatsby-plugin-favicon`,
               options: {
-                logo: "./src/favicon.png",
+                logo: "./src/images/favicon.png",
                 injectHTML: true,
                 icons: {
                   android: true,
