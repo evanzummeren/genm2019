e4723ccc48e1a4a4f018008f5c996c99337b603f
Zaturrby <rjkorteschiel@gmail.com>
2018-06-08 18:44:18 +0200

    little fixes

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,40 +1,42 @@
 import React from 'react'
 import classNames from 'classnames'
-import Link from "gatsby-link"
-import bowser from "bowser"
+import Link from 'gatsby-link'
+import bowser from 'bowser'
 
-import logoSVG from "../../images/logo.svg"
-import logoPNG from "../../images/logo.png"
+import logoSVG from '../../images/logo.svg'
+import logoPNG from '../../images/logo.png'
 
-import stylesGrid from "../pages/landingpage/grid.module.css"
-import stylesHeader from "../pages/landingpage/header.module.css"
+import stylesGrid from '../pages/landingpage/grid.module.css'
+import stylesHeader from '../pages/landingpage/header.module.css'
 const styles = {
   ...stylesGrid,
-  ...stylesHeader
+  ...stylesHeader,
 }
 
-export default function Template({data}) {
-  const { markdownRemark } = data;
-  const { frontmatter, html } = markdownRemark;
+export default ({ data }) => {
+  const { markdownRemark } = data
+  const { frontmatter, html } = markdownRemark
   return (
     <div className={styles.screen}>
       <div
         className={styles.page}
         style={{
           display: 'flex',
-          flexDirection: 'column'
-        }}>
+          flexDirection: 'column',
+        }}
+      >
         <header
           className={classNames(styles.grid, styles.header)}
           style={{
-            flexGrow: 0
-          }}>
-          <Link
-            to="/"
-            >
-            {bowser.msie ?
-              <img className={styles.logo} src={logoPNG}/> :
-              <img className={styles.logo} src={logoSVG}/>}
+            flexGrow: 0,
+          }}
+        >
+          <Link to="/">
+            {bowser.msie ? (
+              <img className={styles.logo} src={logoPNG} />
+            ) : (
+              <img className={styles.logo} src={logoSVG} />
+            )}
           </Link>
         </header>
         <div
@@ -44,23 +46,26 @@ export default function Template({data}) {
             flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
-            padding: '2.4rem 2.4rem 9.6rem'
+            padding: '2.4rem 2.4rem 9.6rem',
           }}
-        > <div
+        >
+          {' '}
+          <div
             style={{
               margin: 0,
-              maxWidth: '50rem'
+              maxWidth: '50rem',
             }}
           >
-            <h1
-              >{frontmatter.title}</h1>
+            <h1>{frontmatter.title}</h1>
             <h3
               style={{
-                maxWidth: '50rem'
+                maxWidth: '50rem',
               }}
-            >{frontmatter.date}</h3>
+            >
+              {frontmatter.date}
+            </h3>
             <div
-              style={{maxWidth: '50rem'}}
+              style={{ maxWidth: '50rem' }}
               dangerouslySetInnerHTML={{ __html: html }}
             />
           </div>
@@ -68,10 +73,10 @@ export default function Template({data}) {
       </div>
     </div>
   )
-};
+}
 
 export const pageQuery = graphql`
-  query MyOwnBlogThingy($path: String!) {
+  query blogQuery($path: String!) {
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
       frontmatter {
@@ -81,4 +86,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
+`
