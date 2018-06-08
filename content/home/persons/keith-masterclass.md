4d22c11ce8e5761dd59755a31a839b23ee7ac9da
Zaturrby <rjkorteschiel@gmail.com>
2018-06-08 19:31:53 +0200

    replace masterclasses

--- content/home/persons/keith-masterclass.md
+++ content/home/persons/keith-masterclass.md
@@ -1,9 +1,10 @@
 ---
-name: Keith Hammodns
+name: Keith Hammonds
 function: Launch your high-impact solutions reporting project
 order: 3
 personType: masterclass
 ---
+
 Drawing from the Solutions Journalism Network’s learning across hundreds of solutions projects from its community of newsrooms and journalists, we’ll frame out key principles for: identifying opportunities for strong solutions projects, finding and vetting solutions story ideas, reporting and structuring solutions stories, connecting solutions stories to audience and community engagement activities. Come with your ideas. We’ll workshop them together, mapping out possible reporting and engagement approaches and, where possible, identifying specific models and sources to anchor your work.
 
 Klik [hier](http://bit.ly/masterclass-genm) om je aan te melden (max 10 personen).
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,63 +1,63 @@
 import React from 'react'
 import classNames from 'classnames'
-import Img from "gatsby-image"
-import { Link } from "react-scroll"
-const ScrollLink = Link;
-import GatsbyLink from "gatsby-link"
-import bowser from 'bowser';
-// import { Timeline } from 'react-twitter-widgets'
+import Img from 'gatsby-image'
+import { Link } from 'react-scroll'
+const ScrollLink = Link
+import GatsbyLink from 'gatsby-link'
+import bowser from 'bowser'
+import { Timeline } from 'react-twitter-widgets'
 
 // Custom scripts
-import generateCalendar from "../components/ical.js"
+import generateCalendar from '../components/ical.js'
 
 // Components
-import Dialog from "../components/Dialog"
-import Card from "../components/Card"
-import Sheet from "../components/Sheet"
-import Map from "../components/Map"
-import Marquee from "../components/Marquee"
-import Button from "../components/Button"
-import ListItem from "../components/ListItem"
+import Dialog from '../components/Dialog'
+import Card from '../components/Card'
+import Sheet from '../components/Sheet'
+import Map from '../components/Map'
+import Marquee from '../components/Marquee'
+import Button from '../components/Button'
+import ListItem from '../components/ListItem'
 
 // Styles
-import stylesGrid from "./landingpage/grid.module.css"
-import stylesHeader from "./landingpage/header.module.css"
-import stylesLanding from "./landingpage/landing.module.css"
-import stylesProgramma from "./landingpage/programma.module.css"
-import stylesLocationcost from "./landingpage/locationcost.module.css"
-import stylesFooter from "./landingpage/footer.module.css"
+import stylesGrid from './landingpage/grid.module.css'
+import stylesHeader from './landingpage/header.module.css'
+import stylesLanding from './landingpage/landing.module.css'
+import stylesProgramma from './landingpage/programma.module.css'
+import stylesLocationcost from './landingpage/locationcost.module.css'
+import stylesFooter from './landingpage/footer.module.css'
 const styles = {
   ...stylesGrid,
   ...stylesHeader,
   ...stylesLanding,
   ...stylesProgramma,
   ...stylesLocationcost,
-  ...stylesFooter
+  ...stylesFooter,
 }
 
 // Images
-import logoSVG from "../../images/logo.svg"
-import logoPNG from "../../images/logo.png"
-import nose from "../../images/nose.svg"
+import logoSVG from '../../images/logo.svg'
+import logoPNG from '../../images/logo.png'
+import nose from '../../images/nose.svg'
 
-const IndexPage = ({data}) => {
-  const headerData = data.general.edges[0].node;
-  const locCostData = data.general.edges[1].node;
-  const footerData = data.general.edges[2].node;
+const IndexPage = ({ data }) => {
+  const headerData = data.general.edges[0].node
+  const locCostData = data.general.edges[1].node
+  const footerData = data.general.edges[2].node
   return (
     <div className={styles.screen}>
       <div className={styles.page}>
         {/* Page header  */}
         <header className={classNames(styles.grid, styles.header)}>
-          <GatsbyLink
-            to="/"
-            >
-            {bowser.msie ?
-              <img className={styles.logo} src={logoPNG}/> :
-              <img className={styles.logo} src={logoSVG}/>}
+          <GatsbyLink to="/">
+            {bowser.msie ? (
+              <img className={styles.logo} src={logoPNG} />
+            ) : (
+              <img className={styles.logo} src={logoSVG} />
+            )}
           </GatsbyLink>
           <div className={styles.nav}>
-            { headerData.frontmatter.navigation.map((navItem, key) => (
+            {headerData.frontmatter.navigation.map((navItem, key) => (
               <ScrollLink
                 className={styles.navItem}
                 key={key}
@@ -72,74 +72,112 @@ const IndexPage = ({data}) => {
 
         {/* Landing */}
         <section className={styles.grid}>
-          <img src={nose} className={styles.headerImg}></img>
+          <img src={nose} className={styles.headerImg} />
           <div className={styles.headerRight}>
-            <h2 className={styles.subtitle}>{headerData.frontmatter.dateLoc}</h2>
+            <h2 className={styles.subtitle}>
+              {headerData.frontmatter.dateLoc}
+            </h2>
             <h1 className={styles.title}>{headerData.frontmatter.title}</h1>
-            <Button text={headerData.frontmatter.button} color="purple" link="http://www.expertisecentrumjournalistiek.nl/agenda/19-juni-2018-de-grote-expertisedag-nieuwe-media/"/>
-            { headerData.frontmatter.details.map((detail, key) => {
-              return (<div className={styles.crumb} key={key}>
-                <p className={styles.crumbText}>{detail}</p>
-              </div>)
+            <Button
+              text={headerData.frontmatter.button}
+              color="purple"
+              link="http://www.expertisecentrumjournalistiek.nl/agenda/19-juni-2018-de-grote-expertisedag-nieuwe-media/"
+            />
+            {headerData.frontmatter.details.map((detail, key) => {
+              return (
+                <div className={styles.crumb} key={key}>
+                  <p className={styles.crumbText}>{detail}</p>
+                </div>
+              )
             })}
           </div>
-          <div className={styles.landingText} dangerouslySetInnerHTML={{ __html: headerData.html }}></div>
+          <div
+            className={styles.landingText}
+            dangerouslySetInnerHTML={{ __html: headerData.html }}
+          />
         </section>
 
         {/* Speakers */}
-        <section className={styles.grid} name={headerData.frontmatter.navigation[0]}>
+        <section
+          className={styles.grid}
+          name={headerData.frontmatter.navigation[0]}
+        >
           <div className={styles.grid24}>
-            <Marquee title="Sprekers"/>
+            <Marquee title="Sprekers" />
           </div>
-          { data.persons.edges.map((person, key) => {
-            return (person.node.frontmatter.personType == "speaker") && (<div className={styles.grid24} key={key}>
-              <Sheet data={person}/>
-            </div>);
+          {data.persons.edges.map((person, key) => {
+            return (
+              person.node.frontmatter.personType == 'speaker' && (
+                <div className={styles.grid24} key={key}>
+                  <Sheet data={person} />
+                </div>
+              )
+            )
           })}
         </section>
 
         {/* CaseStudies */}
-        <section className={styles.grid} name={headerData.frontmatter.navigation[1]}>
+        <section
+          className={styles.grid}
+          name={headerData.frontmatter.navigation[1]}
+        >
           <div className={styles.grid24}>
-            <Marquee title="Case studies"/>
+            <Marquee title="Case studies" />
           </div>
-          { data.persons.edges.map((person, key) => {
-            return (person.node.frontmatter.personType == "casestudy") && (<div className={styles.grid6} key={key}>
-              <Card data={person} key={key}/>
-            </div>);
+          {data.persons.edges.map((person, key) => {
+            return (
+              person.node.frontmatter.personType == 'casestudy' && (
+                <div className={styles.grid6} key={key}>
+                  <Card data={person} key={key} />
+                </div>
+              )
+            )
           })}
         </section>
 
         {/* Viewpoints */}
-        <section className={classNames(styles.grid, styles.gridPaddingLarge)} name={headerData.frontmatter.navigation[2]}>
+        <section
+          className={classNames(styles.grid, styles.gridPaddingLarge)}
+          name={headerData.frontmatter.navigation[2]}
+        >
           <div className={styles.grid24}>
-            <Marquee title="Debat"/>
+            <Marquee title="Debat" />
           </div>
-          { data.persons.edges.map((person, key) => {
-            return (person.node.frontmatter.personType == "debate") && (<div className={styles.grid6} key={key}>
-              <Card data={person} key={key}/>
-            </div>);
+          {data.persons.edges.map((person, key) => {
+            return (
+              person.node.frontmatter.personType == 'debate' && (
+                <div className={styles.grid6} key={key}>
+                  <Card data={person} key={key} />
+                </div>
+              )
+            )
           })}
         </section>
 
         {/* Agenda en Twitter */}
-        {/* <section className={styles.grid} name={headerData.frontmatter.navigation[2]}>
+        <section
+          className={styles.grid}
+          name={headerData.frontmatter.navigation[2]}
+        >
           <div className={styles.grid24}>
-            <Marquee title="Agenda"/>
+            <Marquee title="Agenda" />
           </div>
           <div className={styles.grid12}>
-            { data.agenda.edges.map((event, key) => {
-              return (
-                <ListItem data={event} key={key}/>
-              );
+            {data.agenda.edges.map((event, key) => {
+              return <ListItem data={event} key={key} />
             })}
-            <Button text="Export als iCal" alternative="true" link={generateCalendar(data.agenda.edges)} download={'calendar-genm18.ics'}/>
+            <Button
+              text="Export als iCal"
+              alternative="true"
+              link={generateCalendar(data.agenda.edges)}
+              download={'calendar-genm18.ics'}
+            />
           </div>
-          <div className={classNames(styles.grid12, styles.twitter)}>
+          {/* <div className={classNames(styles.grid12, styles.twitter)}>
             <Timeline
               dataSource={{
                 sourceType: 'widget',
-                widgetId: '986523764660883456'
+                widgetId: '986523764660883456',
               }}
               options={{
                 height: '600',
@@ -147,15 +185,17 @@ const IndexPage = ({data}) => {
                 borderColor: '#d7bda5',
                 linkColor: '#5E358C',
                 dnt: 'true', // Don't collect or use any data from our site.
-                omitScript: 1 // Really, well, don't do anything. Disables updates.
+                omitScript: 1, // Really, well, don't do anything. Disables updates.
               }}
             />
-          </div>
-        </section> */}
-
+          </div> */}
+        </section>
 
         {/* Location and costs */}
-        <section className={classNames(styles.grid, styles.location)} name={headerData.frontmatter.navigation[3]}>
+        <section
+          className={classNames(styles.grid, styles.location)}
+          name={headerData.frontmatter.navigation[3]}
+        >
           <div className={classNames(styles.grid12, styles.map)}>
             <Map
               isMarkerShown
@@ -163,34 +203,38 @@ const IndexPage = ({data}) => {
               zoom={parseFloat(locCostData.frontmatter.locationZoom)}
               location={{
                 lat: parseFloat(locCostData.frontmatter.location.lat),
-                lng: Number(locCostData.frontmatter.location.lng)}}
+                lng: Number(locCostData.frontmatter.location.lng),
+              }}
             />
           </div>
           <div className={classNames(styles.grid12, styles.costs)}>
             <Marquee title="Locatie & Kosten &" secundary={true} />
-            <div dangerouslySetInnerHTML={{ __html: locCostData.html }}></div>
+            <div dangerouslySetInnerHTML={{ __html: locCostData.html }} />
           </div>
         </section>
-
 
         {/* Footer  */}
         <section className={classNames(styles.grid, styles.footer)}>
           <div className={classNames(styles.grid12, styles.affiliates)}>
             <h3 className={styles.affiliatesHeader}>Organisatie</h3>
-            { footerData.frontmatter.logosOrganisatie.map((logo, key) => {
-              return (<div className={styles.affiliateLogo} key={key}>
-                <Img sizes={logo.childImageSharp.sizes} />
-              </div>);
+            {footerData.frontmatter.logosOrganisatie.map((logo, key) => {
+              return (
+                <div className={styles.affiliateLogo} key={key}>
+                  <Img sizes={logo.childImageSharp.sizes} />
+                </div>
+              )
             })}
             <h3 className={styles.affiliatesHeader}>Ondersteund door</h3>
-            { footerData.frontmatter.logosOndersteuning.map((logo, key) => {
-              return (<div className={styles.affiliateLogo} key={key}>
-                <Img sizes={logo.childImageSharp.sizes} />
-              </div>);
+            {footerData.frontmatter.logosOndersteuning.map((logo, key) => {
+              return (
+                <div className={styles.affiliateLogo} key={key}>
+                  <Img sizes={logo.childImageSharp.sizes} />
+                </div>
+              )
             })}
           </div>
           <div className={classNames(styles.grid12, styles.footerInfo)}>
-            <div dangerouslySetInnerHTML={{__html: footerData.html}} />
+            <div dangerouslySetInnerHTML={{ __html: footerData.html }} />
             <div>
               <p>
                 <GatsbyLink to="/blog/privacy/">Privacy Statement</GatsbyLink> -
@@ -202,19 +246,23 @@ const IndexPage = ({data}) => {
       </div>
 
       <div className={classNames(styles.centerer)}>
-        <Dialog button="Bestellen" text={headerData.frontmatter.dialog} buttonLink="http://www.expertisecentrumjournalistiek.nl/agenda/19-juni-2018-de-grote-expertisedag-nieuwe-media/"/>
+        <Dialog
+          button="Bestellen"
+          text={headerData.frontmatter.dialog}
+          buttonLink="http://www.expertisecentrumjournalistiek.nl/agenda/19-juni-2018-de-grote-expertisedag-nieuwe-media/"
+        />
       </div>
-
-    </div>);
-};
+    </div>
+  )
+}
 
 export default IndexPage
 
 export const query = graphql`
   query indexQuery {
     general: allMarkdownRemark(
-      filter: {id: {regex: "//content/home/general//"}},
-      sort: { order: ASC, fields: [frontmatter___order]}
+      filter: { id: { regex: "//content/home/general//" } }
+      sort: { order: ASC, fields: [frontmatter___order] }
     ) {
       edges {
         node {
@@ -252,8 +300,8 @@ export const query = graphql`
       }
     }
     persons: allMarkdownRemark(
-      filter: {id: {regex: "//content/home/persons//"}},
-      sort: { order: ASC, fields: [frontmatter___order]}
+      filter: { id: { regex: "//content/home/persons//" } }
+      sort: { order: ASC, fields: [frontmatter___order] }
     ) {
       edges {
         node {
@@ -285,8 +333,8 @@ export const query = graphql`
       }
     }
     agenda: allMarkdownRemark(
-      filter: {id: {regex: "//content/home/agenda//"}},
-      sort: { order: ASC, fields: [frontmatter___order]}
+      filter: { id: { regex: "//content/home/agenda//" } }
+      sort: { order: ASC, fields: [frontmatter___order] }
     ) {
       edges {
         node {
@@ -304,4 +352,4 @@ export const query = graphql`
       }
     }
   }
-`;
+`
