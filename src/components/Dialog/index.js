2d716a22e5cb078837d01a2ff038d714f39734aa
Erik van Zummeren <erikvanzummeren@gmail.com>
2019-05-03 23:39:28 +0200

    update link

--- src/components/Dialog/index.js
+++ src/components/Dialog/index.js
@@ -8,7 +8,7 @@ import styles from "./index.module.css";
 const Dialog = (props) => (
   <section className={classNames(styles.container, (props.position == "top") && styles.positionTop)}>
     <h4 className={styles.text}>{props.text}</h4>
-    {props.button && <Button text={props.button} alternative="true" link={props.buttonLink}/>}
+    {props.button && <Button text={props.button} alternative="true" link="http://www.expertisecentrumjournalistiek.nl/agenda/18-juni-grote-expertisedag-nieuwe-media-2019/"/>}
   </section>
 )
 
